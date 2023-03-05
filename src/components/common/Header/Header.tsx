ba378e7ab21cec1c1bb860ceb7836542f4ec19f6
Suranjan <suranjanbiswas2002@gmail.com>
2023-03-05 23:23:39 +0530

    add: added custom styles for header

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,141 @@
+import React from "react";
+import Image from "next/image";
+import classnames from "classnames";
+import Link from "next/link";
+import styles from "./Header.module.scss";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from "reactstrap";
+
+const NORMAL_LINKS = [
+  {
+    name: "STABLE Stablecoins",
+    href: "/app/stablecoins",
+    classNames: [styles.LinkText],
+  },
+  {
+    name: "STABLE Bonds",
+    href: "/app/bonds",
+    classNames: [styles.LinkText],
+  },
+];
+
+const AUTH_LINKS = [
+  {
+    name: "Sign In",
+    href: "/auth/signin",
+    classNames: [styles.LinkText],
+  },
+  {
+    name: "Sign Up",
+    href: "/auth/signup",
+    classNames: [styles.LinkText, styles.AuthLinkBgButton],
+  },
+];
+
+const Header: React.FC = () => {
+  const [collapsed, setCollapsed] = React.useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div className={styles.HeaderContainer}>
+      <div className={styles.MobileContainer}>
+        <div>
+          <Navbar color="faded" light>
+            <NavbarBrand
+              style={{
+                paddingLeft: 10,
+              }}
+            >
+              <Image
+                src="/images/header-icon-stable.svg"
+                height={30}
+                width={100}
+                alt="stable-img"
+                priority
+              />
+            </NavbarBrand>
+            <NavbarToggler
+              onClick={toggleNavbar}
+              className="me-2"
+              style={{
+                border: collapsed ? "none" : "1px solid #000",
+                outline: "none",
+                boxShadow: "none",
+              }}
+            />
+            <Collapse isOpen={!collapsed} navbar>
+              <Nav navbar>
+                {NORMAL_LINKS.map((item, index) => (
+                  <NavItem className={styles.NavItemContainer} key={index}>
+                    <Link
+                      href={item.href}
+                      className={classnames([...item.classNames])}
+                    >
+                      {item.name}
+                    </Link>
+                  </NavItem>
+                ))}
+                <Container className={styles.AuthMobileContainer}>
+                  {AUTH_LINKS.map((item, index) => (
+                    <NavItem className={styles.NavItemContainer} key={index}>
+                      <Link
+                        href={item.href}
+                        className={classnames([...item.classNames])}
+                      >
+                        {item.name}
+                      </Link>
+                    </NavItem>
+                  ))}
+                </Container>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      </div>
+      <div className={styles.DesktopContainer}>
+        <section className={styles.IconContainer}>
+          <Image
+            src="/images/header-icon-stable.svg"
+            height={100}
+            width={190}
+            alt="stable-img"
+            priority
+          />
+        </section>
+        <section className={styles.LinksContainer}>
+          {NORMAL_LINKS.map((item, index) => (
+            <Link
+              key={index}
+              href={item.href}
+              className={classnames([...item.classNames])}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </section>
+        <section className={styles.AuthLinkContainer}>
+          {AUTH_LINKS.map((item, index) => (
+            <Link
+              key={index}
+              href={item.href}
+              className={classnames([...item.classNames])}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
