cc5aebb390bda04aff7df849634f971adbfdcc4f
Suranjan <suranjanbiswas2002@gmail.com>
2023-03-26 00:43:15 +0530

    update:  updated the top-container for footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./Footer.module.scss";
 
 const Footer: React.FC = () => {
   return (
     <div className={styles.FooterContainer}>
-      <img src="/images/footer-image.svg" />
+      <section className={styles.TopContainer}>
+        <div className={styles.Content}>
+          <Image
+            src="/images/header-icon-stable.svg"
+            height={100}
+            width={190}
+            alt="stable-img"
+            priority
+          />
+          <div>
+            <a>Stable Protocol</a>
+            <a>Governance</a>
+            <a>Docs</a>
+            <a>Security</a>
+            <a>FAQ</a>
+          </div>
+        </div>
+      </section>
+      <section className={styles.BottomContainer}></section>
     </div>
   );
 };
