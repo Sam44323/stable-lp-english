0e0872f617d0496a443e949cb5a8c0591eed4ac5
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-29 22:18:55 +0530

    add: added container stylings for acquir

--- src/components/section/Acquire/Acquire.tsx
+++ src/components/section/Acquire/Acquire.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 const Acquire = () => {
   return (
-    <div className="w-fit mt-72">
-      <section className="bg-[url('/images/gradients/gradient-bent.svg')] bg-no-repeat min-h-[400px] h-full w-screen">
-        {/* <Image
-          src="/images/gradients/gradient-bent.svg"
-          alt="gradient-bent"
-          height={700}
-          width={1400}
-        /> */}
+    <div className="bg-[url('/images/gradients/gradient-bent.svg')] bg-no-repeat min-h-[800px] h-full w-screen mt-72 pt-36">
+      <section className="text-center font-roboto">
+        <h1 className="text-5xl font-500">Acquire Stablecoins now!</h1>
+        <p className="max-w-2xl mx-auto mt-6 font-300 font-manrope leading-7">
+          Our stablecoins are pegged at a fixed rate of 1:1 with a corresponding
+          fiat currency. For instance, 1 StUSD equates to 1 USD, while 1 StEUR
+          equals EUR. We offer three different ways to acquire our stablecoins:
+        </p>
       </section>
     </div>
   );
