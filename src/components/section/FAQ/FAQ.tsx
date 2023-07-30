e7412628e080b3286f79d02a4eb512252b8df76c
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-30 22:58:51 +0530

    add: added section for faq

--- src/components/section/FAQ/FAQ.tsx
+++ src/components/section/FAQ/FAQ.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const FAQ = () => {
-  return <div className="mt-[400px]">FAQ</div>;
+  return (
+    <div className="mt-[400px] relative">
+      <img
+        src="/images/shapes/faq-ellipse.png"
+        className="absolute left-0 -z-10"
+      />
+      <h1 className="text-center mt-5 text-blue-text font-roboto tracking-wide font-400">
+        FAQ's
+      </h1>
+    </div>
+  );
 };
 
 export default FAQ;
