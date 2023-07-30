22d403a52ec68cb8af48a3a538c58347160df5fa
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-30 15:26:53 +0530

    update: updated the color for stacked-images

--- src/components/section/Joinnow/Joinnow.tsx
+++ src/components/section/Joinnow/Joinnow.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Joinnow = () => {
   return (
-    <div className="gradient-blue-bg 2xl:max-w-screen-2xl max-w-screen-xl mx-auto rounded-3xl md:min-h-[300px]  overflow-hidden relative">
+    <div className="gradient-blue-bg 2xl:max-w-screen-2xl max-w-screen-xl mx-auto rounded-3xl md:min-h-[300px]   relative mt-36">
       <img
         src="/images/shapes/stacked-ellipse.png"
-        className="absolute -left-4 md:max-h-[300px]"
+        className="absolute left-0 md:max-h-[300px] rounded-3xl"
       />
       <img
         src="/images/shapes/right-border-ellipse.png"
