10610e50691fa2d1b1e89d1385836277d592969d
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-30 15:35:11 +0530

    add: added wave-masking for join-now

--- src/components/section/Joinnow/Joinnow.tsx
+++ src/components/section/Joinnow/Joinnow.tsx
@@ -9,8 +9,20 @@ const Joinnow = () => {
       />
       <img
         src="/images/shapes/right-border-ellipse.png"
-        className="absolute right-0 md:max-h-[300px]"
+        className="absolute right-0 md:max-h-[300px] z-10"
       />
+      <div className="relative">
+        <img
+          src="/images/shapes/large-ellipse-blue.png"
+          className="absolute right-0 md:max-h-[300px] rounded-tr-3xl rounded-br-3xl"
+        />
+        <img
+          src="/images/shapes/wave-mask-group.png"
+          height={400}
+          width={400}
+          className="absolute right-[160px] top-32 "
+        />
+      </div>
     </div>
   );
 };
