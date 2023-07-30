ccb758e2340f0834db1f18770e32e95bb2f725f3
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-30 14:53:11 +0530

    add: added container styles for joinnow

--- src/components/section/Joinnow/Joinnow.tsx
+++ src/components/section/Joinnow/Joinnow.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Joinnow = () => {
-  return <div>Joinnow</div>;
+  return (
+    <div className="gradient-blue-bg 2xl:max-w-screen-2xl max-w-screen-xl mx-auto rounded-3xl md:min-h-[300px]  overflow-hidden relative">
+      <img
+        src="/images/shapes/stacked-ellipse.png"
+        className="absolute -left-4 md:max-h-[300px]"
+      />
+    </div>
+  );
 };
 
 export default Joinnow;
