456f3b3cce22c79ccdd733f41696baa0564f5bcd
Suranjan <suranjanbiswas2002@gmail.com>
2023-08-03 03:52:22 +0530

    add: added custom data for container of hero

--- src/components/section/Stablecoins/Hero/Hero.tsx
+++ src/components/section/Stablecoins/Hero/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-row justify-between max-w-screen-xl mx-auto relative z-10 mt-36">
+      <div className="flex flex-row justify-between max-w-screen-lg mx-auto relative z-10 mt-36">
         {STABLECOINS_FEATURES.map((item, index) => (
           <div className="flex flex-col items-center" key={index}>
             <Image src={item.src} alt={item.title} height={190} width={190} />
@@ -47,6 +47,39 @@ const Hero = () => {
             </p>
           </div>
         ))}
+      </div>
+      <div className="relative z-10 mt-36 max-w-5xl mx-auto">
+        <div className="max-w-xl ml-auto">
+          <h1 className="font-roboto text-xl text-blue-text text-right">
+            What can you do with our Stablecoins? 
+          </h1>
+          <p className="font-be-vietnam-pro font-300 mt-4 leading-7 text-sm">
+            Stable's stablecoins are a top choice for crypto enthusiasts, as
+            they can be utilized just like any other fiat-backed stablecoin.
+            However, what sets Stable apart is the added benefit of being able
+            to use them as a medium of exchange for Stables’ RWAs. This means
+            you can effortlessly mint RWAs with them or redeem RWAs for them. To
+            further broaden the usage of stablecoins, Stable is setting up
+            subnets as payment networks with stablecoins as a gas token and with
+            low transaction fees. This will enable retail stores or e-shops to
+            connect to stablecoins and make them easy to use for day-to-day
+            payments.
+          </p>
+        </div>
+        <div className="max-w-xl mt-16">
+          <h1 className="font-roboto text-xl text-blue-text">Regulation</h1>
+          <p className="font-be-vietnam-pro font-300 mt-4 leading-7 text-sm">
+            At Stable, we are committed to upholding the highest compliance
+            standards in order to prevent money laundering, financing of
+            terrorism, and to comply with sanctions and KYC regulations. Prior
+            to being granted access to our mint/redeem feature for stablecoins
+            and our RWA service, all users must successfully pass our AML and
+            KYC checks. Since our headquarters are located in EU, we are proud
+            to be one of the first stablecoin and RWA issuers to be fully
+            regulated in the European Union under MiCA. Above all, the safety of
+            our customers is our top priority.
+          </p>
+        </div>
       </div>
     </div>
   );
