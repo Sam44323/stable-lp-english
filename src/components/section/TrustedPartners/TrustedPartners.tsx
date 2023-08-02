4d93c743aa7b67a807c05e8f510d0f547d2f4e7b
Suranjan <suranjanbiswas2002@gmail.com>
2023-08-02 16:37:33 +0530

    update: updated the image for the partners

--- src/components/section/TrustedPartners/TrustedPartners.tsx
+++ src/components/section/TrustedPartners/TrustedPartners.tsx
@@ -8,7 +8,7 @@ const TrustedPartners = () => {
       </h1>
       <div className="flex flex-row justify-between max-w-6xl mx-auto mt-16">
         <img src="/images/chains/ethereum.png" alt="ethereum-chain" />
-        <img src="/images/chains/polygon.png" alt="polygon-chain" width={200} />
+        <img src="/images/chains/polygon.png" alt="polygon-chain" width={240} />
         <img src="/images/chains/avalanche.png" alt="avalanche-chain" />
       </div>
     </div>
