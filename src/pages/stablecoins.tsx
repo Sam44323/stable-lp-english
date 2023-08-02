eebca18070348b895947f6426cb4d76b7399fd48
Suranjan <suranjanbiswas2002@gmail.com>
2023-08-03 03:27:12 +0530

    add: added page-section for stablecoins

---
+++ src/pages/stablecoins.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "@/components/section/Stablecoins/Hero/Hero";
+import Head from "next/head";
+
+const Stablecoins = () => {
+  return (
+    <>
+      <Head>
+        <title>Stable | Stablecoins</title>
+      </Head>
+      <div>
+        <Hero />
+      </div>
+    </>
+  );
+};
+
+export default Stablecoins;
