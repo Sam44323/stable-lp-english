86806efe62c898888590c916a986c6ee36caae6f
Suranjan <suranjanbiswas2002@gmail.com>
2023-08-06 10:53:23 +0530

    add: added configs for hero

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -13,7 +13,7 @@ const Footer: React.FC = () => {
   return (
     <div className="w-full min-h-[10px] bg-blue-btn">
       <div className="max-w-[1290px] mx-auto pt-14 px-5 pb-10">
-        <section className="flex flex-row justify-between">
+        <section className="flex flex-row justify-between mobile:hidden">
           <img
             src="/images/logo/stable-footer-logo.png"
             height={140}
@@ -31,7 +31,25 @@ const Footer: React.FC = () => {
             ))}
           </div>
         </section>
-        <section className="flex flex-row items-center justify-between">
+        <section className="hidden mobile:flex mobile:flex-col mobile:items-center">
+          <img
+            src="/images/logo/stable-footer-logo.png"
+            height={140}
+            width={140}
+          />
+          <div className="flex justify-between flex-[0.5] mt-10">
+            {TOP_FOOTER_LINK.map((item, index) => (
+              <Link
+                key={index}
+                href={item.link}
+                className="text-white no-underline font-be-vietnam-pro font-300 text-[10px] hover:cursor-pointer hover:bg-blue-pillBtn hover:text-white px-2 py-1 rounded-md transition-all duration-100 ease-in-out "
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </section>
+        <section className="flex flex-row items-center justify-between mobile:hidden">
           <div>
             <p className="text-white font-work-sans my-4 font-300 text-sm">
               Be part of our community!
@@ -52,6 +70,45 @@ const Footer: React.FC = () => {
             </div>
           </div>
           <div className="flex flex-row justify-between flex-[0.5] mt-10">
+            {[FOOTER_LINKS_A, FOOTER_LINKS_B, FOOTER_LINKS_C].map(
+              (item, index) => (
+                <div key={index} className="flex flex-col text-center">
+                  {item.map((link, index) => (
+                    <Link
+                      href={link.link}
+                      key={index}
+                      target="_blank"
+                      className="no-underline font-be-vietnam-pro font-300 text-white my-[10px] text-[12.5px] leading-6 hover:underline"
+                    >
+                      {link.name}
+                    </Link>
+                  ))}
+                </div>
+              )
+            )}
+          </div>
+        </section>
+        <section className="hidden mobile:flex mobile:flex-col mobile:items-center">
+          <div>
+            <p className="text-white font-work-sans my-4 font-300 text-sm">
+              Be part of our community!
+            </p>
+            <div
+              className="flex flex-row justify-between min-w-[180px]
+            "
+            >
+              {FOOTER_IMAGES.map((item, index) => (
+                <Link
+                  href={item.link}
+                  key={index}
+                  className="hover:cursor-pointer hover:scale-110 hover:transition-all duration-100 ease-in-out"
+                >
+                  <Image src={item.src} height={30} width={30} alt={item.alt} />
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-row justify-between min-w-[300px] mt-10">
             {[FOOTER_LINKS_A, FOOTER_LINKS_B, FOOTER_LINKS_C].map(
               (item, index) => (
                 <div key={index} className="flex flex-col text-center">
--- src/components/section/Index/Hero/Hero.tsx
+++ src/components/section/Index/Hero/Hero.tsx
@@ -5,15 +5,15 @@ const Hero: React.FC = () => {
   return (
     <div className="max-w-5xl relative flex flex-row justify-evenly ml-auto mr-auto mt-48">
       <section>
-        <div className="relative">
+        <div className="relative mobile:flex mobile:flex-col mobile:w-full mobile:items-center">
           <Image
             src="/images/shapes/solid-circle-blue.png"
             alt="solid-circle-blue"
             height={100}
             width={100}
-            className="absolute -z-10 -left-16 -top-16"
+            className="absolute -z-10 -left-16 -top-16 mobile:hidden"
           />
-          <h1 className="text-link-inactive tracking-tighter text-6xl font-500 font-roboto leading-[1.4] m-0">
+          <h1 className="text-link-inactive tracking-tighter text-6xl font-500 font-roboto leading-[1.4] m-0 mobile:text-4xl mobile:text-center">
             The easiest way to <br />
             obtain RWA tokens!
           </h1>
@@ -24,9 +24,9 @@ const Hero: React.FC = () => {
             alt="solid-circle-blue"
             height={10}
             width={10}
-            className="absolute -left-16"
+            className="absolute -left-16 mobile:hidden"
           />
-          <p className="font-manrope font-300 w-78 mt-4 leading-[1.6] tracking-wide ml-[7px]">
+          <p className="font-manrope font-300 w-78 mt-4 leading-[1.6] tracking-wide ml-[7px] mobile:ml-0 mobile:text-center">
             Gain access to variety of financial products including government
             bonds, stocks, investment funds, ETF's, and many others.
           </p>
@@ -35,7 +35,7 @@ const Hero: React.FC = () => {
           Get Started
         </div>
       </section>
-      <section className="flex flex-col relative align-end -right-7 mt-16">
+      <section className="flex flex-col relative align-end -right-7 mt-16 mobile:hidden">
         <Image
           src="/images/shapes/masked-circle-pattern.png"
           alt="solid-circle-blue"
