5241717f4cc69fd64c0f223b3ce9c7ef53d23f10
Suranjan <suranjanbiswas2002@gmail.com>
2023-08-06 10:58:48 +0530

    add: added logs for hero

--- src/components/section/Index/Hero/Hero.tsx
+++ src/components/section/Index/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Hero: React.FC = () => {
   return (
-    <div className="max-w-5xl relative flex flex-row justify-evenly ml-auto mr-auto mt-48">
+    <div className="max-w-5xl relative flex flex-row justify-evenly ml-auto mr-auto mt-48 mobile:max-w-[90%] mobile:mt-36 mobile:flex-col">
       <section>
         <div className="relative mobile:flex mobile:flex-col mobile:w-full mobile:items-center">
           <Image
@@ -13,7 +13,7 @@ const Hero: React.FC = () => {
             width={100}
             className="absolute -z-10 -left-16 -top-16 mobile:hidden"
           />
-          <h1 className="text-link-inactive tracking-tighter text-6xl font-500 font-roboto leading-[1.4] m-0 mobile:text-4xl mobile:text-center">
+          <h1 className="text-link-inactive tracking-tighter text-6xl font-500 font-roboto leading-[1.4] m-0 mobile:text-3xl mobile:text-center">
             The easiest way to <br />
             obtain RWA tokens!
           </h1>
@@ -26,12 +26,12 @@ const Hero: React.FC = () => {
             width={10}
             className="absolute -left-16 mobile:hidden"
           />
-          <p className="font-manrope font-300 w-78 mt-4 leading-[1.6] tracking-wide ml-[7px] mobile:ml-0 mobile:text-center">
+          <p className="font-manrope font-300 max-w-xl mt-4 leading-[1.6] tracking-wide ml-[7px] mobile:ml-0 mobile:text-center mobile:text-sm">
             Gain access to variety of financial products including government
             bonds, stocks, investment funds, ETF's, and many others.
           </p>
         </div>
-        <div className="pill-btn hover:ease-in duration-100 hover:scale-105 min-w-[190px]">
+        <div className="pill-btn hover:ease-in duration-100 hover:scale-105 min-w-[190px] mobile:max-w-50% mobile:mx-auto">
           Get Started
         </div>
       </section>
