982fec6eb423806ddd0b3f4505e4a8cec1c00937
Suranjan <suranjanbiswas2002@gmail.com>
2023-08-07 22:16:24 +0530

    add: added mobile-page structure for rwa

--- src/components/section/RWA-Page/Hero/Hero.tsx
+++ src/components/section/RWA-Page/Hero/Hero.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -7,23 +6,25 @@ const Hero = () => {
       <img
         src="/images/shapes/rwa-mesh-right.png"
         alt="solid-circle-blue"
-        className="absolute right-0 top-36"
+        className="absolute right-0 top-36 mobile:hidden"
       />
-      <div className="max-w-5xl mx-auto pt-48">
-        <h1 className="text-link-inactive tracking-tighter text-4xl max-w-lg font-500 font-roboto leading-[1.4] m-0">
+      <div className="max-w-5xl mx-auto pt-48 mobile:text-center">
+        <h1 className="text-link-inactive tracking-tighter text-4xl max-w-lg font-500 font-roboto leading-[1.4] m-0 mobile:text-xl">
           Tokenize your assets and obtain more control and flexibility over
           them.
         </h1>
-        <div className="pill-btn hover:ease-in duration-100 hover:scale-105 min-w-[190px] mt-5">
-          Gain Access to RWA
+        <div className="mobile:flex mobile:flex-row mobile:justify-center">
+          <div className="pill-btn hover:ease-in duration-100 hover:scale-105 min-w-[190px] mt-5">
+            Gain Access to RWA
+          </div>
         </div>
       </div>
       <div className="relative z-10 mt-16 max-w-5xl mx-auto">
         <div className="max-w-xl">
-          <h1 className="font-roboto text-xl text-blue-text">
+          <h1 className="font-roboto text-xl text-blue-text mobile:text-center">
             How Does It Work?
           </h1>
-          <p className="font-be-vietnam-pro font-300 mt-4 leading-7 text-sm">
+          <p className="font-be-vietnam-pro font-300 mt-4 leading-7 text-sm mobile:text-center mobile:max-w-[90%] mobile:mx-auto">
             The RWA tokenizing mechanism on Stable platform is executed through
             a dedicated smart-contract for each RWA. Participants may use Stable
             stablecoins (StUSD, StEUR, etc.) to interact with a specific
@@ -33,7 +34,7 @@ const Hero = () => {
             the selected asset.
           </p>
         </div>
-        <p className="max-w-xl ml-auto mt-16 font-be-vietnam-pro font-300 leading-7 text-sm">
+        <p className="max-w-xl ml-auto mt-16 font-be-vietnam-pro font-300 leading-7 text-sm mobile:text-center mobile:max-w-[90%] mobile:mx-auto">
           The RWA tokenizing mechanism on Stable platform is executed through a
           dedicated smart-contract for each RWA. Participants may use Stable
           stablecoins (StUSD, StEUR, etc.) to interact with a specific contract
@@ -43,11 +44,18 @@ const Hero = () => {
           asset.
         </p>
       </div>
-      <Image
+      <img
         src="/images/rwa-flow.png"
-        className="max-w-screen-lg mx-auto mt-36 object-fill"
+        className="max-w-screen-lg mx-auto mt-36 mobile:hidden"
         height={500}
         width={1200}
+        alt="redeem-flow"
+      />
+      <img
+        src="/images/rwa-flow.png"
+        className="hidden max-w-screen-lg mx-auto mt-36 mobile:block"
+        height={100}
+        width={300}
         alt="redeem-flow"
       />
     </div>
--- src/components/section/RWA-Page/HowToGet/HowToGet.tsx
+++ src/components/section/RWA-Page/HowToGet/HowToGet.tsx
@@ -3,20 +3,20 @@ import Supportedchains from "@/components/common/Supportedchains/Supportedchains
 
 const HowToGet = () => {
   return (
-    <div className="bg-[url('/images/gradients/gradient-bent.svg')] bg-no-repeat bg-cover 2xl:bg-cover 2xl:pt-[5%]  mt-72 pt-16 mx-auto relative pb-40">
+    <div className="bg-[url('/images/gradients/gradient-bent.svg')] bg-no-repeat bg-contain 2xl:bg-cover 2xl:pt-[5%]  mt-72 pt-36 mx-auto relative pb-40 mobile:pb-10">
       <div className="relative z-10 mt-16 max-w-5xl mx-auto">
         <div className="max-w-xl">
-          <h1 className="font-roboto text-xl text-blue-text">
+          <h1 className="font-roboto text-xl text-blue-text mobile:text-center">
             How to get RWAs?
           </h1>
-          <p className="font-be-vietnam-pro font-300 mt-4 leading-7 text-sm">
+          <p className="font-be-vietnam-pro font-300 mt-4 leading-7 text-sm mobile:text-center mobile:max-w-[90%] mobile:mx-auto">
             To access Stable's Real World Assets (RWAs), users must acquire
             Stable's stablecoins initially. Once users have stablecoins, they
             can utilize them for RWAs minting or redeeming by interacting with
             dedicated smart-staking.
           </p>
         </div>
-        <p className="max-w-xl ml-auto mt-16 font-be-vietnam-pro font-300 leading-7 text-sm">
+        <p className="max-w-xl ml-auto mt-16 font-be-vietnam-pro font-300 leading-7 text-sm mobile:text-center mobile:max-w-[90%] mobile:mx-auto">
           This process involves burning the stablecoins, buying assets for
           backing, and minting an RWA token. With a focus on transparency and
           1:1 asset backing, each RWA token is fully backed by an equivalent
