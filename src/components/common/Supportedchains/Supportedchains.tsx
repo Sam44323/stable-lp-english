90781a1d698be1de540377629c81fa6b89dea5cd
Suranjan <suranjanbiswas2002@gmail.com>
2023-08-11 16:31:45 +0530

    update: updated the chains

--- src/components/common/Supportedchains/Supportedchains.tsx
+++ src/components/common/Supportedchains/Supportedchains.tsx
@@ -8,62 +8,70 @@ const Supportedchains = () => {
       </h1>
       <div className="flex flex-row justify-between max-w-[1190px] mx-auto mt-16 mobile:hidden">
         <img
-          src="/images/chains/ethereum.png"
+          src="/images/chains/ethereum.svg"
           alt="ethereum-chain"
           style={{
-            height: "50px",
-            width: "190px",
-          }}
-        />
-        <img
-          src="/images/chains/polygon.png"
-          alt="polygon-chain"
-          style={{
-            height: "50px",
+            height: "200px",
             width: "200px",
           }}
         />
         <img
-          src="/images/chains/arbitrum.png"
-          alt="arbitrum-chain"
+          src="/images/chains/polygon.svg"
+          alt="polygon-chain"
           style={{
-            height: "50px",
-            width: "190px",
+            height: "200px",
+            width: "200px",
           }}
         />
         <img
-          src="/images/chains/avalanche.png"
+          src="/images/chains/arbitrum.svg"
+          alt="arbitrum-chain"
+          style={{
+            height: "200px",
+            width: "200px",
+          }}
+        />
+        <img
+          src="/images/chains/avalanche.svg"
           alt="avalanche-chain"
           style={{
-            height: "50px",
-            width: "240px",
+            height: "200px",
+            width: "200px",
           }}
         />
       </div>
       <div className="mobile:flex mobile:flex-col mobile:justify-between mobile:align-middle mobile:w-fit mobile:mx-auto mobileL:mt-16 hidden">
         <img
-          src="/images/chains/ethereum.png"
+          src="/images/chains/ethereum.svg"
           alt="ethereum-chain"
-          width={240}
+          style={{
+            width: "200px",
+          }}
           className="mt-10"
         />
         <img
-          src="/images/chains/polygon.png"
+          src="/images/chains/polygon.svg"
           alt="polygon-chain"
-          width={240}
+          style={{
+            width: "200px",
+          }}
           className="mobile:mt-10"
         />
         <img
-          src="/images/chains/arbitrum.png"
+          src="/images/chains/arbitrum.svg"
           alt="arbitrum-chain"
-          width={240}
+          style={{
+            width: "200px",
+          }}
           className="mt-10"
         />
         <img
-          src="/images/chains/avalanche.png"
+          src="/images/chains/avalanche.svg"
           alt="avalanche-chain"
-          width={240}
-          className="mobile:mt-10"
+          style={{
+            width: "200px",
+          }}
+          className="mobile:mt-5"
         />
       </div>
     </section>
