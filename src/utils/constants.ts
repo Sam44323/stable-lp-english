d037fa712fa6e0724869007a0864ea441f7391b9
Suranjan <suranjanbiswas2002@gmail.com>
2023-09-03 19:34:11 +0530

    update: updated the text

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -109,26 +109,26 @@ export const FAQ_DATA = [
   {
     title: "Why is Stable introducing its own stablecoins?",
     description:
-      "At Stable, we are committed to offering the simplest, quickest, safest, and most transparent tokenization model, which is pushing us toward issuing our own fully regulated, licensed, and transparent fiat-backed stablecoins so that we can offer the following benefits: stablecoins based on currencies other than the US dollar for global asset tokenization, fast and convenient tokenization, no third party systematic risks, transparent audits coupled with Proof of Reserve reports.  ",
+      "At Stable, we are committed to offering the simplest, quickest, safest, and most transparent tokenization model, which is pushing us toward issuing our own fully regulated, licensed, and transparent fiat-backed stablecoins so we are not reliant solely on third-party stablecoin issuers. By that, we have the opportunity to offer fast (almost instant) and safe tokenization with no third-party systematic risks, and transparent audits coupled with Proof of Reserve reports. However we will still support other stablecoins for minting and redeeming, but it will be more expensive and it will take more time to tokenize the chosen asset.",
   },
   {
     title: "How is Stable tapping the potential of blockchain?",
     description:
-      "At Stable, we are taping the potential of blockchain through Tokenization, which offers enhance liquidity, stability, transparency, and accessibility of conventional financial assets while enabling users to take full control of their tokenized assets and unleash their maximum potential.",
+      "We are taping the potential of blockchain through Tokenization, which offers enhanced liquidity, stability, transparency, and accessibility of conventional financial assets while enabling users to take control of their tokenized assets and unleash their maximum potential.",
   },
   {
     title:
       "How is Stable maintaining the pegs and what makes our holding trustable for stablecoins and RWA’s?",
     description:
-      "Stablecoins are hard pegged to the value of 1 to backing currency when minting and redeeming, and the backing is directly reported through Chainlink Proof of Reserve reports. The securities and assets that we tokenize (RWAs) will adhere to the same standards as our stablecoin when it comes to audits and proof of reserve reporting. A ratio of one-to-one will once again back each RWA token. Price feeds for our stablecoins and RWA tokens are provided by Chainlink to ensure maximum accuracy and safety. If there will be an on-chain depeg event, the peg can be restored thanks to Arbitrageurs and friendly Market making funds buying on-chain and redeeming stablecoins for a value of 1 or RWA for off-chain market price, or vice versa.",
+      "Each stablecoins are hard pegged to the value of 1 to the backing currency when minting and redeeming, and the backing is directly reported through Chainlink Proof of Reserve reports. The listed securities and assets that we tokenize (RWAs) will adhere to the same standards as our stablecoins when it comes to audits and Proof of Reserve reporting. A ratio of one-to-one will once again back each RWA token. Price feeds for our stablecoins and RWAs tokens are provided by Chainlink to ensure maximum accuracy and safety. If there is an on-chain depeg event, the peg can be restored thanks to Arbitrageurs buying de-pegged stablecoins and RWAs on-chain and redeeming them for peg value, or vice versa.",
   },
   {
     title:
       " What are some of the major solutions which Stable is bringing to the market?",
     description: [
-      "Stable's objective is to broaden the range of fiat-backed stablecoins by introducing stUSD, stEUR, stCNY, and more, enabling users to diversify their stablecoin holdings and facilitating the development of a fully functional forex market on the blockchain. Moreover, these stablecoins will offer instant usability by granting access to the tokenization of Real-World Assets (RWAs), enhancing their value proposition.",
-      "Through our RWA system, we aim to tokenize real-world assets and securities, creating a seamless and accessible platform for buying, selling, and gaining exposure to these valuable assets.",
-      "As part of our tokenization service, users will receive RWA tokens, ERC tokens directly backed and linked to the specific asset they have bought with our stablecoins. These tokens will mirror the asset's price on the open market of assets that are backing them 1:1, thanks to Chainlink's Oracle integration. To further enhance the utility of our RWA tokens, we plan to collaborate with industry-leading lending, CDP, and DEX protocols, allowing them to be utilized as collateral or in a liquidity pool.",
+      "Our objective is to broaden the range of fiat-backed stablecoins by introducing stUSD, stEUR, stCNY, and more, enabling users to diversify their stablecoin holdings, and facilitating the development of a fully functional forex market on the blockchain. Moreover, these stablecoins will offer instant usability for the fast (almost instant) tokenization of Real-World Assets (RWAs), enhancing their value proposition.",
+      "Through our RWA system, we aim to tokenize real-world assets and listed securities, creating a seamless and accessible platform for buying, selling, and gaining exposure to these valuable assets.",
+      "As part of our tokenization service, users will receive RWA tokens, ERC tokens directly backed and linked to the specific asset they have bought (minted). These tokens will mirror the price on the open market of the assets that are backing them 1:1, thanks to Chainlink's Oracle integration. To further enhance the utility of our RWA tokens, we plan to collaborate with industry-leading lending, CDP, and DEX protocols, allowing them to be utilized in Defi.",
     ],
   },
   {
