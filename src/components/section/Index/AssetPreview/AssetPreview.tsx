86a676b4767f7a3ce78c0d278aa9692c7245d118
Suranjan <suranjanbiswas2002@gmail.com>
2023-10-01 03:29:47 +0530

    fix: fixed the padding data for asst

--- src/components/section/Index/AssetPreview/AssetPreview.tsx
+++ src/components/section/Index/AssetPreview/AssetPreview.tsx
@@ -3,7 +3,7 @@ import AssetPreviewCard from "./AssetPreviewCard";
 
 const AssetPreview = () => {
   return (
-    <div className="bg-[url('/images/gradients/gradient-bent.svg')] bg-[center_top_5rem] overflow-x-hidden bg-no-repeat bg-cover 2xl:bg-cover large-screen:bg-cover mobile:bg-contain mt-10 relative z-30 mobile:mt-16">
+    <div className="bg-[url('/images/gradients/gradient-bent.svg')] bg-[center_top_5rem] pb-96 bg-no-repeat bg-cover 2xl:bg-cover large-screen:bg-cover mobile:bg-cover mt-10 relative z-30 mobile:mt-16">
       <section className="max-w-[1200px] mx-auto flex justify-between mobile:flex-col mobile:items-center">
         <AssetPreviewCard
           title="stSGOV"
