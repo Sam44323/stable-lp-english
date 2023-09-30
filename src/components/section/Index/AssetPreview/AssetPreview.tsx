9f85d1d75e08be6ec47a2832679abc51220d5f4e
Suranjan <suranjanbiswas2002@gmail.com>
2023-10-01 02:40:50 +0530

    uupdate: updated the props structure for data

--- src/components/section/Index/AssetPreview/AssetPreview.tsx
+++ src/components/section/Index/AssetPreview/AssetPreview.tsx
@@ -5,7 +5,15 @@ const AssetPreview = () => {
   return (
     <div className="bg-[url('/images/gradients/gradient-bent.svg')] overflow-x-hidden bg-no-repeat bg-cover 2xl:bg-cover large-screen:bg-cover  2xl:pt-[5%] mt-4 pt-36 mx-auto relative pb-40 mobile:pb-10 mobile:pt-16 z-10">
       <section>
-        <AssetPreviewCard />
+        <AssetPreviewCard
+          title="stSGOV"
+          status="loaded"
+          description="Tokenized iShares 0-3 Month Treasury Bond ETF"
+          price="100"
+          yieldData="0.01%"
+          change="2%"
+          changeType="positive"
+        />
       </section>
     </div>
   );
