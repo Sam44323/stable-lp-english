6207dcbbc4b840dbfa9fd851dfdd59662603f961
Suranjan <suranjanbiswas2002@gmail.com>
2023-10-01 17:11:48 +0530

    update: updated the logics for the flex

--- src/components/section/Index/AssetPreview/AssetPreview.tsx
+++ src/components/section/Index/AssetPreview/AssetPreview.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import AssetPreviewCard from "./AssetPreviewCard";
 
 const AssetPreview = () => {
+  // pb-[400px]
   return (
-    <div className="bg-[url('/images/gradients/gradient-bent.svg')] bg-[center_top_5rem] pb-96 bg-no-repeat bg-cover 2xl:bg-cover large-screen:bg-cover mobile:bg-cover mt-10 relative z-30 mobile:mt-16">
+    <div className="bg-[url('/images/gradients/gradient-bent.svg')] bg-[center_top_5rem] pb-48 bg-no-repeat bg-cover 2xl:bg-cover large-screen:bg-cover mobile:bg-cover mt-10 relative z-30 mobile:mt-16">
       <section className="max-w-[1200px] mx-auto flex justify-between mobile:flex-col mobile:items-center">
         <AssetPreviewCard
           title="stSGOV"
@@ -42,7 +43,38 @@ const AssetPreview = () => {
           changeType="positive"
         />
       </section>
-      <p>nice</p>
+      <section className="mt-36 flex justify-between align-middle max-w-[1300px] mx-auto mobile:flex-col mobile:items-center mobile:mt-16">
+        <div className="flex flex-col items-center justify-between text-center">
+          <h1 className="text-center text-xl text-blue-text font-500 font-roboto max-w-[86%] mx-auto mb-10">
+            Your gateway to global financial and capital markets
+          </h1>
+          <p className="text-grey-100 text-sm font-400 leading-6 max-w-[80%] mx-auto">
+            You are no longer dependant on silly Mon-Fri 9am-5pm trading hours.
+            24/7 liquidity and transferability granted by machines and
+            smart-contracts.
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-center justify-between">
+          <h1 className="text-center text-xl text-blue-text font-500 font-roboto max-w-[60%] mx-auto mb-10">
+            Collateralize, trade, lend and self-custody
+          </h1>
+          <p className="text-grey-100 text-sm font-400 leading-6 max-w-[80%] mx-auto">
+            Traditional securities now becoming available worldwide,
+            permissionless. Harvest all benefits of blockchain that traditional
+            markets are incapable of.
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-between text-center">
+          <h1 className="text-center text-xl text-blue-text font-500 font-roboto max-w-[70%] mx-auto mb-10">
+            Redeem for stablecoins
+          </h1>
+          <p className="text-grey-100 text-sm font-400 leading-6 max-w-[80%] mx-auto">
+            Any verified holder of the tokenized asset can de-tokenize and
+            redeem back for stablecoins for off-chain market price and not rely
+            on DEX or CEX liquidity.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
