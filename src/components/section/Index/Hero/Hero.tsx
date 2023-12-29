f5ec74514b496aa8edb78267ea82a51107e6ae42
Suranjan <suranjanbiswas2002@gmail.com>
2023-12-29 22:59:22 +0530

    update: updated the values for configs

--- src/components/section/Index/Hero/Hero.tsx
+++ src/components/section/Index/Hero/Hero.tsx
@@ -12,10 +12,11 @@ const Hero: React.FC = () => {
             alt="solid-circle-blue"
             height={110}
             width={110}
-            className="absolute -z-10 -left-[100px] top-0 mobile:hidden"
+            className="absolute -z-10 -left-[200px] -top-16 mobile:hidden"
           />
-          <h1 className="text-link-inactive tracking-tight -top-10 text-[60px] absolute mobile:relative mobile:max-w-[100px] min-w-[1000px] mobile:min-w-full font-500 font-roboto leading-[1.1] m-0 mobile:text-3xl mobile:text-center">
-            Listed securities from traditional markets, Now on-chain!
+          <h1 className="text-link-inactive tracking-tight -top-10 text-[60px] absolute mobile:relative mobile:max-w-[100px] min-w-[750px] large-screen:min-w-[800px] max-w-[800px] mobile:min-w-full font-500 font-roboto leading-[1.1] m-0 mobile:text-3xl mobile:text-center">
+            Combining traditional market securities with DeFi’s on Chain
+            Flexibility.
           </h1>
         </div>
         <div>
@@ -24,16 +25,11 @@ const Hero: React.FC = () => {
             alt="solid-circle-blue"
             height={15}
             width={15}
-            className="absolute -left-0 mobile:hidden top-[250px]"
+            className="absolute -left-[50px] mobile:hidden top-[200px]"
           />
-          <p className="font-work-sans mobile:min-w-full font-300 max-w-xl mt-36 mobile:mt-10 text-grey-100 leading-[1.4] text-md tracking-wide ml-[7px] mobile:ml-0 mobile:text-center mobile:text-sm">
-            Bring your preferred Equities, Bonds, ETFs, and other instruments to
-            your favorite blockchain, unleash their maximum potential, and
-            elevate your financial possibilities with the best blockchain and
-            Defi offer. All you need to do is mint the assets you intend to hold
-            with whitelisted fiat-backed stablecoins such as USDC and USDT or
-            use our in-house Stablecoins for the quickest and simplest minting
-            and redemption experience.
+          <p className="font-work-sans mobile:min-w-full font-300 max-w-xl mt-[250px] mobile:mt-10 text-grey-100 leading-[1.4] text-md tracking-wide ml-[7px] mobile:ml-0 mobile:text-center mobile:text-sm">
+            Unleash the maximum potential of your preferred Equities, Bonds,
+            ETFs and more with the power of your chosen blockchain.
           </p>
         </div>
       </section>
