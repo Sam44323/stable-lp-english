002fc40b9150eb2f4e272eb4cd155df29d451e7d
Suranjan <suranjanbiswas2002@gmail.com>
2023-12-31 01:10:13 +0530

    update: updated the hero

--- src/components/section/RWA-Page/Hero/Hero.tsx
+++ src/components/section/RWA-Page/Hero/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         className="absolute right-0 top-48 mobile:hidden"
       />
       <div className="max-w-6xl mx-auto pt-48 mobile:text-center mobile:pt-20">
-        <h1 className="text-link-inactive tracking-tighter text-[50px] max-w-3xl font-500 font-roboto leading-[70px] m-0 mobile:text-xl">
+        <h1 className="text-link-inactive tracking-tight text-[50px] max-w-3xl font-500 font-roboto leading-[70px] m-0 mobile:text-xl">
           Enjoy greater control and flexibility over your chosen assets through
           tokenization.
         </h1>
@@ -26,32 +26,34 @@ const Hero = () => {
         </div>
       </div>
       <div className="relative z-10 mt-16 max-w-6xl mx-auto mid-screen:max-w-none mobile:max-w-none">
-        <div className="max-w-xl mid-screen:max-w-none mobile:max-w-none">
+        <div className="max-w-[90%] mid-screen:max-w-none mobile:max-w-none">
           <h1 className="font-roboto text-3xl text-blue-text mid-screen:text-center mobile:text-center mobile:text-xl">
             How Does It Work?
           </h1>
-          <p className="font-be-vietnam-pro font-300 mt-4 text-grey-100 min-w-[700px] mobile:min-w-0 leading-8 text-lg mobile:text-center mobile:max-w-[90%] mobile:mx-auto mobile:text-sm mobile:leading-9">
-            The RWA tokenization is executed through a dedicated smart contract
-            for each RWA. To mint your favorite tokenized equity, bond, ETF, or
-            other instruments, you need to deposit whitelisted fiat-backed
-            stablecoins such as USDT, USDC or stablecoins from Stable to a
-            specific contract representing a particular asset in a specific
-            region, leading to the burning of the stablecoins, acquiring the
-            assets, and minting RWA token to you. Note that accepted stablecoins
-            differ for each asset. Fees and tokenization times vary based on
-            which stablecoin you are using. Stablecoins from Stable are the
-            cheapest in fees (no third-party stablecoin issuers fees) and the
-            fastest in the time needed to finish the tokenization process.
+          <p className="font-be-vietnam-pro font-300 mt-4 text-grey-100 min-w-[1200px] mobile:min-w-0 leading-8 text-[18px] mobile:text-center mobile:max-w-[90%] mobile:mx-auto mobile:text-sm mobile:leading-9">
+            RWA tokenization is executed through a dedicated smart contract per
+            RWA. To mint your chosen tokenized equity, bond, ETF, or other
+            instrument, deposit whitelisted fiat-backed stablecoins such as
+            USDT, USDC or STABLEcoins from Stable to a specific contract
+            representing a particular asset in a specific region, leading to
+            burning of the stablecoins, acquiring the assets, and minting RWA
+            token to you. <br /> <br />
+            Note that accepted stablecoins differ for each asset. Fees and
+            tokenization speed vary based on which stablecoins are used. <br />{" "}
+            <br />{" "}
+            <span className="font-500">
+              STABLEcoins from STABLE are optimum in both fees (no third-party
+              stablecoin issuers fees) and in speed of tokenization process.
+            </span>
+            <br /> <br />
+            When choosing to redeem, the asset is sold on the open market, after
+            which the corresponding stablecoins are minted to you based on
+            proceeds received. Each asset is 1:1 backed and reported through
+            Chainlink’s Proof of Reserves, ensuring complete transparency and
+            security. Fees and redemption time vary based on the stablecoin for
+            which you are redeeming the RWA token.
           </p>
         </div>
-        <p className="max-w-xl ml-auto mt-16 font-be-vietnam-pro font-300 text-grey-100 min-w-[700px] leading-8 text-lg mobile:min-w-0 mobile:text-center mobile:max-w-[90%] mobile:mx-auto mobile:text-sm mobile:leading-9">
-          Conversely, when you choose to redeem, the asset is sold on the open
-          market, after which the corresponding stablecoins are minted to you
-          based on the proceeds received. Again, each asset is 1:1 backed and
-          reported through Chainlink Proof of Reserves, thus ensuring complete
-          transparency and security. Yet again, fees and redemption time vary
-          based on for which stablecoin you are redeeming the RWA token.
-        </p>
       </div>
     </div>
   );
