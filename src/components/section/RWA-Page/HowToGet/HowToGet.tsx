66c3896778589f1ad95f41ec392fe2ddf994993f
Suranjan <suranjanbiswas2002@gmail.com>
2023-12-31 01:17:37 +0530

    update: updated the methods

--- src/components/section/RWA-Page/HowToGet/HowToGet.tsx
+++ src/components/section/RWA-Page/HowToGet/HowToGet.tsx
@@ -7,29 +7,23 @@ const HowToGet = () => {
 
   return (
     <div className="bg-[url('/images/gradients/gradient-bent.svg')] bg-no-repeat bg-contain 2xl:bg-cover 2xl:pt-[5%]  mt-0 pt-16 mx-auto relative pb-36 mobile:pb-10 mobile:mt-48">
-      <div className="relative z-10 mt-16 max-w-5xl mx-auto">
-        <div className="max-w-xl mid-screen:max-w-none mobile:max-w-none">
-          <h1 className="font-roboto text-3xl text-blue-text mobile:text-center mobile:text-xl">
-            How to get RWAs?
-          </h1>
-          <p className="font-be-vietnam-pro font-300 mt-4 text-grey-100 min-w-[800px] mobile:min-w-0  leading-8 text-lg mobile:text-center mobile:max-w-[90%] mobile:mx-auto mobile:text-sm mobile:leading-9">
-            To be able to mint, redeem or act as arbitrageur for Stable's Real
-            World Assets (RWAs), you must first create an account with us and
-            successfully complete our KYC and AML checks. Once it’s all done,
-            you are free to unleash the maximum potential of RWAs.
-          </p>
-        </div>
-        <p className="max-w-xl ml-auto mt-16 font-be-vietnam-pro font-300 text-grey-100 min-w-[800px] mobile:min-w-0  leading-8 text-lg mobile:text-center mobile:max-w-[90%] mobile:mx-auto mobile:text-sm mobile:leading-9">
-          The actual process involves burning the stablecoins, buying the assets
-          for backing, and minting the RWA token. With a focus on transparency
-          and 1:1 asset backing, each RWA token is fully backed by an equivalent
-          amount of assets that is representing. This means that 100 StTSLA
-          tokens are backed by 100 TSLA shares in Stable's custody, which is
-          reported through Chainlinks{" "}
-          <span className="font-600">Proof of Reserve.</span>
+      <div className="relative z-10 mt-16 max-w-5xl mx-auto text-center">
+        <h1 className="font-roboto text-3xl text-blue-text mobile:text-center mobile:text-xl text-center">
+          RWA Minting Process
+        </h1>
+        <p className="font-be-vietnam-pro font-300 mt-4 text-grey-100 min-w-[800px] mobile:min-w-0  leading-8 text-lg mobile:text-center mobile:max-w-[90%] mobile:mx-auto mobile:text-sm mobile:leading-9">
+          To mint, redeem or act as arbitrageur for Stable’s Real World Assets
+          (RWAs), first create a Stable account and successfully complete all
+          KYC and AML checks. The process involves burning the stablecoins,
+          purchasing assets for backing, and minting the RWA token. <br />{" "}
+          <br /> With a dual focus on transparency and 1:1 asset backing, each
+          RAW token is backed by an equivalent amount assets that it represents.
+          This means that 100 StTSLA tokens are backed by 100 TSLA shares in
+          Stable custody, reported through Chainlink’s{" "}
+          <span>Proof of Reserve.</span>
         </p>
       </div>
-      <div className="gradient-blue-bg 2xl:max-w-screen-2xl max-w-[1200px] mx-auto rounded-3xl md:min-h-[200px] overflow-clip relative mt-48 mobile:my-10 mobile:max-w-[90%] mobile:overflow-hidden">
+      <div className="gradient-blue-bg 2xl:max-w-screen-2xl max-w-[1200px] mx-auto rounded-3xl md:min-h-[200px] overflow-clip relative mt-36 mobile:my-10 mobile:max-w-[90%] mobile:overflow-hidden">
         <div>
           <img
             src="/images/shapes/stacked-ellipse.png"
@@ -42,7 +36,7 @@ const HowToGet = () => {
               Collateralize, trade, lend, and self-custody
             </h1>
             <p className="text-white font-roboto font-500 text-[18px] ml-1 tracking-wide mid-screen:text-sm mobile:text-sm">
-              Sign up and unlock the capabilities ...
+              Join Stable to discover the power of Defi...
             </p>
           </div>
           <div
