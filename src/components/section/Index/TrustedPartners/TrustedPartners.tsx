3ce08c50a3564825a5e92f55e05bd114adca08ec
Suranjan <suranjanbiswas2002@gmail.com>
2023-12-31 14:29:20 +0530

    update: updated the styles for trusted-partners

--- src/components/section/Index/TrustedPartners/TrustedPartners.tsx
+++ src/components/section/Index/TrustedPartners/TrustedPartners.tsx
@@ -6,7 +6,7 @@ const TrustedPartners = () => {
       <h1 className="text-center text-roboto text-blue-text text-xl  font-700 lg:text-[40px]">
         Trusted Partners
       </h1>
-      <div className="flex flex-col lg:flex-row items-center justify-evenly mt-16">
+      <div className="flex flex-col lg:flex-row items-center justify-evenly mt-16 max-w-[1400px] mx-auto">
         <img
           src="/images/confirmo.png"
           className="mb-16 h-10 lg:h-auto lg:mb-0"
