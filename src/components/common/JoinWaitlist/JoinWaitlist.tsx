a536c507b8f97a01ade0f1794504569ff7dc534a
Suranjan <suranjanbiswas2002@gmail.com>
2023-12-31 21:12:15 +0530

    update: updated the modal

--- src/components/common/JoinWaitlist/JoinWaitlist.tsx
+++ src/components/common/JoinWaitlist/JoinWaitlist.tsx
@@ -7,6 +7,7 @@ import { toast } from "react-toastify";
 const JoinWaitlist = () => {
   const [showModal, setShowModal] = React.useState<boolean>(false);
   const [email, setEmail] = React.useState<string>("");
+  const [confirmedSignup, setConfirmedSignup] = React.useState<boolean>(false);
 
   React.useEffect(() => {
     setTimeout(() => {
@@ -25,8 +26,9 @@ const JoinWaitlist = () => {
       });
       if (response.status === 200) {
         toast.dismiss();
-        toast.success("You have been added to our waitlist");
-        setShowModal(false);
+        // toast.success("You have been added to our waitlist");
+        setConfirmedSignup(true);
+        setTimeout(() => setShowModal(false), 3000);
         setEmail("");
       }
     } catch (err: any) {
@@ -41,9 +43,11 @@ const JoinWaitlist = () => {
       isOpen={showModal}
       centered
       toggle={() => setShowModal(false)}
-      size="lg"
+      size="xl"
       style={{
         overflow: "hidden",
+        maxWidth: "900px",
+        width: "100%",
       }}
     >
       <div className="relative overflow-hidden">
@@ -63,63 +67,71 @@ const JoinWaitlist = () => {
             className="hover:cursor-pointer"
           />
         </div>
-        <div className="relative z-10 px-10 py-10">
+        <div className="relative z-10 px-16 py-10">
           <div>
-            <h1 className="text-white font-robot font-600 text-5xl leading-[50px] mobile:text-3xl mobile:text-center">
-              Subscribe & <br /> Don't Miss a Thing!
+            <h1 className="text-white font-robot font-600 text-[50px] leading-[50px] mobile:text-3xl mobile:text-center">
+              {confirmedSignup
+                ? "Delighted to have you on board!"
+                : "Subscribe to pioneer the future of tokenized assets with Stable!"}
             </h1>
-            <section className="my-4">
-              <h1 className="text-white font-be-vietnam-pro text-3xl font-300 mobile:text-xl mobile:text-center">
-                Join Our Testing
-              </h1>
+            <section className="mt-4 mb-14">
+              {!confirmedSignup && (
+                <h1 className="text-white font-be-vietnam-pro text-3xl font-300 mobile:text-xl mobile:text-center">
+                  Limited Stable tester spots available.
+                </h1>
+              )}
               <p className="text-white font-be-vietnam-pro text-base mt-3 font-100 max-w-[80%] ml-0  mobile:text-center mobile:max-w-[100%]">
-                We would like to invite you to be one of the first to try out
-                our new product. To do so, we would like to add you to our
-                waiting list for the beta-release.
+                {confirmedSignup
+                  ? "Welcoming you as a Stable Defi pioneer  - one of the first to try out our transformational platform!"
+                  : "Join our early adopter community, be the first to try out the revolutionary future of finance and get the latest updates on Stable’s developments."}
               </p>
             </section>
-            <div className="flex flex-row justify-between mobile:hidden">
-              <input
-                placeholder="Enter your email"
-                type="email"
-                className="
+            {!confirmedSignup && (
+              <div className="flex flex-row justify-between mobile:hidden">
+                <input
+                  placeholder="Enter your email"
+                  type="email"
+                  className="
                 border-1 border-white bg-transparent rounded-full py-[14px] px-4 font-roboto font-400 tracking-wide
                 text-white text-base w-[70%] placeholder-slate-50
                 "
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <button
-                onClick={handleJoinWaitlist}
-                className="
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+                <button
+                  onClick={handleJoinWaitlist}
+                  className="
               bg-white rounded-full py-[14px] px-3 font-robot text-sm font-700 tracking-wide
               text-black
               "
-              >
-                Subscribe & Join Testing
-              </button>
-            </div>
-            <div className="hidden mobile:flex mobile:flex-col mobile:align-middle mobile:justify-center">
-              <input
-                placeholder="Enter your email"
-                type="email"
-                className="
+                >
+                  Subscribe & Join Testing
+                </button>
+              </div>
+            )}
+            {!confirmedSignup && (
+              <div className="hidden mobile:flex mobile:flex-col mobile:align-middle mobile:justify-center">
+                <input
+                  placeholder="Enter your email"
+                  type="email"
+                  className="
                 border-1 border-white bg-transparent rounded-full py-[14px] px-4 font-roboto font-400 tracking-wide
                 text-white text-base w-[70%] placeholder-slate-50 mobile:w-[100%] mobile:mb-4
                 "
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <button
-                onClick={handleJoinWaitlist}
-                className="
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+                <button
+                  onClick={handleJoinWaitlist}
+                  className="
               bg-white rounded-full py-[14px] px-3 font-robot text-sm font-700 tracking-wide
               text-black
               "
-              >
-                Subscribe & Join Testing
-              </button>
-            </div>
+                >
+                  Subscribe & Join Testing
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
